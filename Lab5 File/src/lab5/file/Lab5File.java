a9cc8f3267d1c143f3b8e0ec1e13be07f2dd2475
Christopher1078 <christortizfun@gmail.com>
2026-02-27 10:44:56 -0600

    Main

--- Lab5 File/src/lab5/file/Lab5File.java
+++ Lab5 File/src/lab5/file/Lab5File.java
@@ -7,7 +7,8 @@ public class Lab5File {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        ConsolaCMD CMD=new ConsolaCMD();
+        CMD.setVisible(true);
     }
     
 }
